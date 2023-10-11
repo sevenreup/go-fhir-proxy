654f6c11fa12994f494c1cd7d4a8542b377c693f
Christopher Seven Phiri <chris7phiri@gmail.com>
2023-10-12 00:06:47 +0200

    setup simple request handling

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go-fhir-proxy
-simple Fhir proxy written in golang
+
+Simple Fhir proxy written in golang
+Trying to recreate [fhir gateway](https://github.com/google/fhir-gateway) in golang.
+
+This is proof of concept and will just implement some small subset of the functionality
--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/samply/golang-fhir-models/fhir-models/fhir"
 )
 
 const (
@@ -22,45 +28,121 @@ func getLoadBalanceAddr() string {
 	return RealAddr[0]
 }
 
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+		c.Next()
+		latency := time.Since(t)
+		log.Print(latency)
+		status := c.Writer.Status()
+		log.Println(status)
+	}
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/:path", func(c *gin.Context) {
-		req := c.Request
-		proxy, err := url.Parse(getLoadBalanceAddr())
-		if err != nil {
-			log.Printf("error in parse addr: %v", err)
-			c.String(500, "error")
-			return
-		}
-		req.URL.Scheme = proxy.Scheme
-		req.URL.Host = proxy.Host
-		path, err := url.JoinPath(proxy.Path, req.URL.Path)
-		if err != nil {
-			log.Printf("error in join path: %v", err)
-			c.String(500, "error")
-			return
-		}
-		req.URL.Path = path
-
-		transport := http.DefaultTransport
-		resp, err := transport.RoundTrip(req)
-		if err != nil {
-			log.Printf("error in roundtrip: %v", err)
-			c.String(500, "error")
-			return
-		}
-
-		fmt.Println(req, proxy)
-		fmt.Println(resp)
-
-		for k, vv := range resp.Header {
-			for _, v := range vv {
-				c.Header(k, v)
-			}
-		}
-		defer resp.Body.Close()
-		bufio.NewReader(resp.Body).WriteTo(c.Writer)
-		return
+	r.Use(Logger())
+	r.GET("*path", func(c *gin.Context) {
+		handleRequest(c)
 	})
 	r.Run(ReverseServerAddr)
+}
+
+func handleRequest(c *gin.Context) {
+	req := c.Request
+	proxy, err := url.Parse(getLoadBalanceAddr())
+	if err != nil {
+		log.Printf("error in parse addr: %v", err)
+		c.String(500, "error")
+
+	}
+	isBundleReq := len(strings.Split(req.URL.Path, "/")) > 2
+	req.URL.Scheme = proxy.Scheme
+	req.URL.Host = proxy.Host
+	path, err := url.JoinPath(proxy.Path, req.URL.Path)
+	if err != nil {
+		log.Printf("error in join path: %v", err)
+		c.String(500, "error")
+		return
+	}
+	req.URL.Path = path
+
+	transport := http.DefaultTransport
+	resp, err := transport.RoundTrip(req)
+	if err != nil {
+		log.Printf("error in roundtrip: %v", err)
+		c.String(500, "error")
+		return
+	}
+
+	for k, vv := range resp.Header {
+		for _, v := range vv {
+			c.Header(k, v)
+		}
+	}
+	defer resp.Body.Close()
+	data, err := handleResponseBody(resp, isBundleReq)
+	if err != nil {
+		log.Printf("error in handle request: %v", err)
+		c.String(500, "error")
+		return
+	}
+	bufio.NewReader(data).WriteTo(c.Writer)
+}
+
+func handleResponseBody(r *http.Response, isBundleReq bool) (rd io.Reader, err error) {
+	if err != nil {
+		return nil, fmt.Errorf("error in parse addr: %v", err)
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+	err = r.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("error closing body: %v", err)
+	}
+
+	if isBundleReq {
+		return bytes.NewReader(body), nil
+	}
+
+	bundle := &fhir.Bundle{}
+	if err := json.Unmarshal(body, &bundle); err != nil {
+		log.Fatal(err)
+	}
+
+	for i, entry := range bundle.Entry {
+		fullUrl := serverToProxyUrl(entry.FullUrl)
+		bundle.Entry[i].FullUrl = &fullUrl
+	}
+
+	for i, link := range bundle.Link {
+		fullUrl := serverToProxyUrl(&link.Url)
+		bundle.Link[i].Url = fullUrl
+	}
+
+	b, err := json.Marshal(bundle)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return bytes.NewReader(b), nil
+}
+
+func serverToProxyUrl(requstUrl *string) string {
+	fullUrl, err := url.Parse(*requstUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fullUrl.Host = ReverseServerAddr
+	parts := strings.Split(fullUrl.Path, "/")
+	path, err := url.JoinPath("", parts[2:]...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullUrl.Path = path
+
+	return fullUrl.String()
 }
